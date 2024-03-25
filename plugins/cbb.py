ba7d073a18272165840193c00cadd5ce2726810e
yujiitadori230 <164171521+yujiitadori230@users.noreply.github.com>
2024-03-25 17:23:54 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={6821495886}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Official website : <a href='https://dramaflixtamilofficial.blogspot.com'>DramaFlixTv Official {__version__}</a>\n○ Channel : @DramaFlixTamil\n○ Support Group : @DramaFlixTamil</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
